ed928dc4ad529c878344468b595778e9f1364172
Matti Ezzat <matti.ezzat@wlw.de>
2016-06-07 09:01:24 +0200

    Add the tab

---
+++ recipe_box/app/controllers/recipes_controller.rb
@@ -0,0 +1,52 @@
+class RecipesController < ApplicationController
+  before_action :find_recipe, only: [:show, :edit]
+  def index
+    @recipe = Recipe.all.order("created_at DESC")
+  end
+
+  def show
+  end
+
+  def new
+    @recipe = Recipe.new
+  end
+
+  def create
+    @recipe = Recipe.new(recipe_params)
+
+    if @recipe.save
+      redirect_to @recipe, notice: "Successfully created a recipe"
+    else
+      render "new"
+    end
+  end
+
+  def edit
+
+  end
+
+  def update
+    @recipe = Recipe.find(params[:id])
+    if @recipe.update(recipe_params)
+      redirect_to @recipe
+    else
+      render 'edit'
+    end
+  end
+
+  def destroy
+    @recipe = Recipe.find(params[:id])
+    @recipe.destroy
+    redirect_to root_path, notice: "Successfully deleted"
+  end
+
+  private
+
+  def recipe_params
+    params.require(:recipe).permit(:title, :description)
+  end
+
+  def find_recipe
+    @recipe = Recipe.find(params[:id])
+  end
+end
